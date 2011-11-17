3814b18feb8ba09d904a2d5060ed539a4721f764
Andrew Harry <andyharry@bigpond.com>
2011-11-17 10:41:31 +1030

    Initial Build

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// AssemblyInfo.cs
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ScriptSharpKnockout")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScriptSharpKnockout")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ScriptAssembly("ScriptSharpKnockout")]
